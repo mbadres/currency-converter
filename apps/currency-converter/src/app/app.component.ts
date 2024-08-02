956aa5c502d2975c2b58c557f722aaf6e713d70f
Miena Basta Badres <dev@mbadres.com>
2024-08-02 17:03:46 +0200

    Modularize the Angular app.

--- apps/currency-converter/src/app/app.component.ts
+++ apps/currency-converter/src/app/app.component.ts
@@ -1,14 +1,15 @@
 import { Component } from '@angular/core';
 import { RouterModule } from '@angular/router';
+import { ConverterComponent } from "./converter/converter.component";
+import { FooterComponent } from "./footer/footer.component";
+import { HeaderComponent } from "./header/header.component";
+import { HistoryComponent } from "./history/history.component";
 import { NxWelcomeComponent } from './nx-welcome.component';
 
 @Component({
   standalone: true,
-  imports: [NxWelcomeComponent, RouterModule],
+  imports: [NxWelcomeComponent, RouterModule, FooterComponent, HeaderComponent, HistoryComponent, ConverterComponent],
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrl: './app.component.css',
 })
-export class AppComponent {
-  title = 'Currency Converter';
-}
+export class AppComponent {}
---
+++ apps/currency-converter/src/app/converter/converter.component.ts
@@ -0,0 +1,10 @@
+import { CommonModule } from '@angular/common';
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-converter',
+  standalone: true,
+  imports: [CommonModule],
+  templateUrl: './converter.component.html',
+})
+export class ConverterComponent {}
